cc92bf84ed2c562ea573adc6d6bb7d69dbfb83ed
lauraeyo <lauraeyob@gmail.com>
2022-02-03 11:02:52 +0100

    Ficheros relacionados con la capa modelo y capa servicios añadidos

---
+++ CMSTFG/src/main/java/com/tfg/CmstfgApplication.java
@@ -0,0 +1,13 @@
+package com.tfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmstfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmstfgApplication.class, args);
+	}
+
+}
